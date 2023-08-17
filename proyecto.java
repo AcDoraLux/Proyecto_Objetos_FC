e219e2efc3432ea60e062ba4e8faf48d9ebcb8f0
AcDoraLux <cobenaangel7@gmail.com>
2023-08-17 17:01:22 -0500

    probar2

--- proyecto.java
+++ proyecto.java
@@ -6,5 +6,7 @@ public class proyecto {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
         System.out.println("sd");
+        System.out.println("Rata");
+        System.out.println(3);
     }
 }
